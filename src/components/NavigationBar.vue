f162009ae35efd5398ae230c0555e3990f39a49f
Natnael Demelash <natidemelash074@gmail.com>
2022-11-20 16:10:16 +0300

    Navigation bar and Search input

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row item-center gap-4 py-6">
+      <RouterLink :to="{ name: home }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">Local Weather App</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex flex-1 justify-end gap-5">
+        <i
+          @click="toggleModal"
+          class="fa-solid fa-circle-info text-2xl cursor-pointer"
+        ></i>
+        <i class="fa-solid fa-plus text-2xl cursor-pointer"></i>
+      </div>
+
+      <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+        <h1 class="text-2xl mb-1">About:</h1>
+        <p class="mb-4">
+          The Local Weather allows you to track the current and future weather
+          of cities of your choosing.
+        </p>
+        <h2 class="text-2xl">How it works:</h2>
+        <ol class="list-decimal list-inside mb-4">
+          <li>
+            Search for your city by entering the name into the search bar.
+          </li>
+          <li>
+            Select a city within the results, this will take you to the current
+            weather for your selection.
+          </li>
+          <li>
+            Track the city by clicking on the "+" icon in the top right. This
+            will save the city to view at a later time on the home page.
+          </li>
+        </ol>
+
+        <h2 class="text-2xl">Removing a city</h2>
+        <p>
+          If you no longer wish to track a city, simply select the city within
+          the home page. At the bottom of the page, there will be am option to
+          delete the city.
+        </p>
+      </BaseModal>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+import BaseModal from "../components/BaseModal.vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
+</script>
